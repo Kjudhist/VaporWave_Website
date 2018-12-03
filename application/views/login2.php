25d003e7825682edbc0e472a29cf2b2dbc119a1c
Kevin JG <dev.kjudhist@gmail.com>
2018-12-04 03:56:16 +0700

    Update login2.php

--- application/views/login2.php
+++ application/views/login2.php
@@ -6,7 +6,7 @@
   <meta name="author" content="">
   <link rel="icon" href="../../favicon.ico">
 
-  <title>VaporWave</title>
+  <title>Sistem Informasi Persewaan Kamar</title>
 
   <!-- Bootstrap core CSS -->
   <link href="<?php echo base_url() ?>dist/css/bootstrap.min.css" rel="stylesheet">
